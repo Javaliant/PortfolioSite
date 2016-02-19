522d7221c7d1c465fa303b0a1d3961b173e0df70
Luigi Vincent <luigipvincent@gmail.com>
2016-02-18 23:38:48 -0500

    reformat html

--- contact.php
+++ contact.php
@@ -12,21 +12,21 @@
 		<link rel="shortcut icon" href="assets/icon.ico"/>
 	</head>
 
-	<header>
-		<span id="name">Luigi Vincent / Software Architect</span>
-		<span><a href="index.html">
-			<img id="home" src="assets/home.png" onmouseover="hover(this);" onmouseout="unhover(this);" alt="Home">
-		</a></span>
-		<ul class="navigation">
-			<li><a class="navitrast" href="about.html">ABOUT</a></li>
-			<li><a href="contact.php">CONTACT</a></li>
-		</ul>
-		<input type="image" id="scrollbutton" src="assets/scroll_arrow.png" onmouseover="scrollHover(this);" onmouseout="scrollUnhover(this);" onclick="scrollUp();"/>
-		<hr color=#666664>
-	</header>
-	
 	<body>
-		<form style="text-align: center;" method:"post" action="contact.php">
+		<header>
+			<span id="name">Luigi Vincent / Software Architect</span>
+			<span><a href="index.html">
+				<img id="home" src="assets/home.png" onmouseover="hover(this);" onmouseout="unhover(this);" alt="Home">
+			</a></span>
+			<ul class="navigation">
+				<li><a class="navitrast" href="about.html">ABOUT</a></li>
+				<li><a href="contact.php">CONTACT</a></li>
+			</ul>
+			<input type="image" id="scrollbutton" src="assets/scroll_arrow.png" onmouseover="scrollHover(this);" onmouseout="scrollUnhover(this);" onclick="scrollUp();"/>
+			<hr color=#666664>
+		</header>	
+	
+		<form style="text-align: center;" method="post" action="contact.php">
 			<div><span class="label">Subject</span><br><input class="input" name="subject" type="text" required="required"></div>
 			<div><span class="label">Email</span><br><input class="input" name="email" type="text" required="required"></div>
 			<div><span class="label">Message</span><br><textarea class="input" name="message" rows="10" cols="80"></textarea></div>
@@ -37,27 +37,26 @@
 				$subject = $_POST['subject'];
 				$email = $_POST['email'];
 				$message = $_POST['message'];
-				$to = 'luigipvincent@gmail+website.com';
-				$body = "From: $email\n Message:\n $message"
+				$to = 'luigipvincent+website@gmail.com';
+				$body = "From: $email\n Message:\n $message";
 				 if (mail($to, $subject, $body)) {
-			        $result = 'Thank you. I will be in touch.';
+			        echo 'Thank you. I will be in touch.';
 			    } else {
-			        $result = 'Sorry, there was an error, please try again later.';
+			        echo 'Sorry, there was an error, please try again later.';
 			    }
 			}
-			echo $result;
 		?>
-	</body>
 
-	<footer>
-		<hr color=#666664>
-		<div id="contacts">
-		<a title="Code Review Profile" target="_blank" href="http://codereview.stackexchange.com/users/49181/legato"><img src="assets/codereview_icon.png"></a>
-		<a title="Github Profile" target="_blank" href="https://github.com/Javaliant"><img src="assets/github_icon.png"></a>
-		<a title="Hacker Rank Profile" target="_blank" href="https://www.hackerrank.com/Legato"><img src="assets/hacker_rank_icon.png"/></a>
-		<a title="Codeeval Profile" target="_blank" href="https://www.codeeval.com/profile/Javaliant/"><img src="assets/codeeval_icon.png"></a>
-		<a title="LinkedIn Profile" target="_blank" href="https://www.linkedin.com/in/luigi-vincent-b82254103"><img src="assets/linked_in_icon.png"></a>
-		</div>
-		<div id="copyright">© 2016 Luigi Vincent</div>
-	</footer>
+		<footer>
+			<hr color=#666664>
+			<div id="contacts">
+			<a title="Code Review Profile" target="_blank" href="http://codereview.stackexchange.com/users/49181/legato"><img src="assets/codereview_icon.png"></a>
+			<a title="Github Profile" target="_blank" href="https://github.com/Javaliant"><img src="assets/github_icon.png"></a>
+			<a title="Hacker Rank Profile" target="_blank" href="https://www.hackerrank.com/Legato"><img src="assets/hacker_rank_icon.png"/></a>
+			<a title="Codeeval Profile" target="_blank" href="https://www.codeeval.com/profile/Javaliant/"><img src="assets/codeeval_icon.png"></a>
+			<a title="LinkedIn Profile" target="_blank" href="https://www.linkedin.com/in/luigi-vincent-b82254103"><img src="assets/linked_in_icon.png"></a>
+			</div>
+			<div id="copyright">© 2016 Luigi Vincent</div>
+		</footer>
+	</body>
 </html>
