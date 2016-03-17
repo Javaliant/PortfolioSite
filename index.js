3844c8d84de1f52b1e1be30bd17de2e6fca33d9e
Luigi Vincent <luigipvincent@gmail.com>
2016-03-17 12:16:00 -0400

    update index.js

--- index.js
+++ index.js
@@ -70,6 +70,22 @@ var website = new Project(
 	], "PortfolioSite"
 );
 
+var webChatter = new Project(
+	"A web-based chat client-server application powered by Node.js.", [
+		"Powered by JavaScript, Node.JS, Express.JS, Socket.IO, HTML, CSS and MySQL."
+	], [
+		"To further enhance knowledge of and employ features of Node.JS",
+		"To create a web-based version of a previously created desktop application using completely different technologies",
+		"Made to illustrate the principles of socket connections, server-side scripting and communications to peers.",
+		"For the sake of practice and learning."
+	], [
+		"The strengths of Node.JS and how to recognize its appropriate and optimal usage.",
+		"The applicability of express.js and Socket.IO in supplementing node based applications. "
+		"How to provide security in applications where using PHP would allow vulnerabalities.",
+		"How to manage MySQL connections and queries via JavaScript."
+	], "webChatter"
+);
+
 var htmlSaver = new Project(
 	"HTML Saver - A tool to locally save a site's source.", [
 		"Creating utilizing Java, JavaFX, CSS and XML."
@@ -162,6 +178,47 @@ var violinTuner = new Project(
 	], "ViolinTuner"
 );
 
+var takeFlight = new Project(
+	"A key event example.", [
+		"Alters a gif animation based on keyboard presses.",
+		"Created using Java, JavaFX."
+	], [
+		"To illustrate Key Events and the fun and power of JavaFX.",
+		"A simple example to engage students/peers in programming.",
+		"For the sake of practice and learning."
+	], [
+		"The simplicity of key based events.",
+		"How to employ different visual effects.",
+		"Wisdom into the capacity of several animation classes in JavaFX."
+	], "TakeFlight"
+);
+
+var challenges = new Project(
+	"Myriad programming challenges solved.", [
+		"List of several concepts encountered and overcome.",
+		"Predominantly completed with Java, Python, Go, Rust and C.",
+	], [
+		"To practice and further hone skills.",
+		"To introduce self to different methods and design paradigms employed in a variety of languages."
+	], [
+		"Many considerations in performance and the cost of certain data structures.",
+		"Countless realizations leading to many refactorings and more best practice pradigms.",
+		"Recognizing the complexity and effectiveness of algorithms."
+	], "Challenges"
+);
+
+var calculator = new Project(
+	"Several calculators, translators and generators", [
+		"From permutation calculators, diploma generators, morse code translators, to simple calculators.",
+		"Predominantly completed with Java, JavaFX as well as C, MATLAB, JavaScript.",
+	], [
+		"For the sake of practice and learning.",
+		"To take skills and apply it to something that eases a real world tasks."
+	], [
+		"Lambda calculus, functional expressions, and archetypal language structure acumen.",
+	], "NewCalculator"
+);
+
 window.addEventListener('load', init, false);
 
 function init() {
@@ -226,6 +283,18 @@ function projectDescribe(project) {
 		break;
 		case "violin-tuner":
 			violinTuner.showInformation();
+		break;
+		case "web-chatter":
+			webChatter.showInformation();
+		break;
+		case "challenges":
+			challenges.showInformation();
+		break;
+		case "calculator":
+			calculator.showInformation();
+		break;
+		case "take-flight":
+			takeFlight.showInformation();
 		break;
 	}
 }
